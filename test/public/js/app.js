c91d38c733cffd2f3de2ce5fc5cb9057c705360a
Daniel Potapov <potapovdaniel@gmail.com>
2019-05-26 22:56:24 -0500

    Use default route in html test

--- test/public/js/app.js
+++ test/public/js/app.js
@@ -1,10 +1,12 @@
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
 const FooBar = { template: '<div>foo.bar</div>' }
+const PageNotFound = { template: '<h2>404 - Page Not Found</h2>' }
 const routes = [
     { path: '/foo', component: Foo },
     { path: '/bar', component: Bar },
     { path: '/foo.bar', component: FooBar },
+    { path: '*', component: PageNotFound },
 ]
 const router = new VueRouter({
     mode: 'history',
@@ -15,6 +17,6 @@ var app = new Vue({
     el: '#app',
     router: router,
     data: {
-    message: 'Hello Vue!'
+        message: 'Hello Vue!'
     }
 })
